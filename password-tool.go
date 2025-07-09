b72c44bca9ad746f074c446f6f422d8c0b9ce719
NarsU-Githbb <anssi.peltoniemi@hotmail.com>
2025-07-09 15:13:36 +0300

    Add Feature Defining Function For The Randomizer
    Made a function that sends uppercase / lowercase letters, numbers & symbols for the randomizer function to use.
    
    Next step: make the ramdomizer function logic so that the function will return a password that's completely random
    & has at least 12 letters / uses at least one from each categories listed above!

--- password-tool.go
+++ password-tool.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	//"flag"
+	"fmt"
+)
+
+func main() {
+
+	upper := []rune{}
+	for i := 'A'; i <= 'Z'; i++ {
+		upper = append(upper, i)
+	}
+
+	lower := []rune{}
+	for i := 'a'; i <= 'z'; i++ {
+		lower = append(lower, i)
+	}
+
+	numnum := []rune{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}
+	symbol := []rune{'@', '#', '$', '%', '^', '&', '*', '_', '-', '+', '=', '|', ':', ';', '<', '>', '.', '?', '/', ','}
+
+	fmt.Println(string(upper))
+	fmt.Println(string(lower))
+	fmt.Println(string(numnum))
+	fmt.Println(string(symbol))
+
+	randomizer(lower, upper, numnum, symbol)
+}
+
+func randomizer(lower, upper, numnum, symbol []rune) string {
+
+}
